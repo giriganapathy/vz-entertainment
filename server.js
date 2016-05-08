de37ca6b0ed96fed5a76e8cc99211846ab8b0d4e
giriganapathy <giriganapathy@gmail.com>
2016-05-08 15:01:13 +0530

    second commit
    Updated the app Id and secret keys.

--- server.js
+++ server.js
@@ -192,7 +192,8 @@ bot.add("/showOffer", [
 dialog.onDefault(builder.DialogAction.send("I am sorry, i did not understand your answser"));
 //bot.listenStdin();
 var server = restify.createServer();
-server.use(bot.verifyBotFramework());
+//server.use(bot.verifyBotFramework());
+server.use(bot.verifyBotFramework({ appId: process.env.appId, appSecret: process.env.appSecret }));
 server.post("/api/messages", bot.listen());
 server.listen(process.env.port, function () {
     console.log("%s listening to %s", server.name, server.url);
