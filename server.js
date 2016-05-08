5dea646ffad92bdd84fbd22bdc413f7f880944a5
giriganapathy <giriganapathy@gmail.com>
2016-05-08 16:19:46 +0530

    fifth comment

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ the customers by showing the Verizon offers and helping them to order placements
 -----------------------------------------------------------------------------*/
 var restify = require("restify");
 var builder = require("botbuilder");
+
 var model = process.env.model || "https://api.projectoxford.ai/luis/v1/application?id=588f2ecf-6889-42a9-9e42-6e345e692543&subscription-key=b27a7109bc1046fb9cc7cfa874e3f819&q=";
 var modelUri = "https://api.projectoxford.ai/luis/v1/application?id=588f2ecf-6889-42a9-9e42-6e345e692543&subscription-key=b27a7109bc1046fb9cc7cfa874e3f819";
+
 var dialog = new builder.LuisDialog(model);
+
 var bot = new builder.BotConnectorBot();
+
 var currentOffer = "FiOS Triple Play";
+
+
+
 bot.use(function (session, next) {
     if (!session.userData.userNameReceivedFlag) {
         session.userData.userNameReceivedFlag = true;
@@ -22,11 +29,13 @@ bot.use(function (session, next) {
         next();
     }
 });
+
 bot.add("/userProfile", [
     function (session) {
         builder.Prompts.text(session, "May i know your name please?");
     },
     function (session, results) {
+
         builder.LuisDialog.recognize(session.message.text, modelUri, function (err, intents, entities) {
             var entity = builder.EntityRecognizer.findEntity(entities, 'name');
             if (null != entity) {
@@ -40,7 +49,9 @@ bot.add("/userProfile", [
         });
     }
 ]);
+
 //bot.add("/", dialog);
+
 bot.add("/showOffer", [
     function (session, args, next) {
         if (!session.userData.selectedOffer) {
@@ -70,13 +81,16 @@ bot.add("/showOffer", [
         if (results.response && !session.userData.zipCode) {
             var zipAndState = "";
             var zipCode = "";
+
             var userResponse = results.response;
+
             var zipAndStatePattern = "\\s\\w{2}\\s\\d{5}";
             var regExpZipAndStatePattern = new RegExp(zipAndStatePattern);
             if (regExpZipAndStatePattern.test(userResponse)) {
                 var zipAndStateArr = regExpZipAndStatePattern.exec(userResponse);
                 if (null != zipAndStateArr) {
                     zipAndState = zipAndStateArr[0];
+
                     var zipCodePattern = new RegExp("\\d{5}");
                     var zipCodeArr = zipCodePattern.exec(zipAndState);
                     if (null != zipCodeArr) {
@@ -121,7 +135,7 @@ bot.add("/showOffer", [
             }
             else {
                 next({ response: session.message.text });
-            }
+            }            
         }
     },
     function (session, results) {
@@ -135,46 +149,48 @@ bot.add("/showOffer", [
                             var reply = new builder.Message()
                                 .setText(session, captionText)
                                 .addAttachment({
-                                text: "50/50 Mbps Internet + Custom TV + Phone starting at $79.99/mo",
-                                title: session.userData.selectedOffer + " offer for TV",
-                                titleLink: "http://www.verizon.com/home/fiostv/",
-                                contentType: "image/jpeg",
-                                contentUrl: "http://www.verizon.com/cs/groups/public/documents/adacct/mob_ocw_ftp_ofr2_0403.png"
-                            });
-                            //session.send("50/50 Mbps Internet + Custom TV + Phone starting at $79.99/mo");
+                                    text: "50/50 Mbps Internet + Custom TV + Phone starting at $79.99/mo",
+                                    title: session.userData.selectedOffer + " offer for TV",
+                                    titleLink: "http://www.verizon.com/home/fiostv/",
+                                    contentType: "image/jpeg",
+                                    contentUrl: "http://www.verizon.com/cs/groups/public/documents/adacct/mob_ocw_ftp_ofr2_0403.png"                                    
+                                });
+                                //session.send("50/50 Mbps Internet + Custom TV + Phone starting at $79.99/mo");
                             session.send(reply);
                             builder.Prompts.confirm(session, "Do you like to proceed?\nPlease confirm " + session.userData.name);
                             break;
-                        case "internet":
+                        case "internet":                            
                             var captionText = "We have found a perfect offer for you. Please check this.\n";
                             var reply = new builder.Message()
                                 .setText(session, captionText)
                                 .addAttachment({
-                                text: "150/150 Mbps Internet + Custom TV + Phone starting at $89.99/mo",
-                                title: session.userData.selectedOffer + " offer for Internet",
-                                titleLink: "http://www.verizon.com/home/fios-fastest-internet/",
-                                contentType: "image/jpeg",
-                                contentUrl: "http://www.verizon.com/cs/groups/public/documents/adacct/mob_ocw_ftp_ofr2_0403.png"
-                            });
+                                    text: "150/150 Mbps Internet + Custom TV + Phone starting at $89.99/mo",
+                                    title: session.userData.selectedOffer + " offer for Internet",
+                                    titleLink: "http://www.verizon.com/home/fios-fastest-internet/",
+                                    contentType: "image/jpeg",
+                                    contentUrl: "http://www.verizon.com/cs/groups/public/documents/adacct/mob_ocw_ftp_ofr2_0403.png"                                    
+                                });
                             //session.send("50/50 Mbps Internet + Custom TV + Phone starting at $79.99/mo");
                             session.send(reply);
-                            builder.Prompts.confirm(session, +"Do you like to proceed?\nPlease confirm " + session.userData.name);
+                            builder.Prompts.confirm(session, + "Do you like to proceed?\nPlease confirm " + session.userData.name);
                             break;
+
                         case "phone":
                             var captionText = "We have found a perfect offer for you. Please check this.\n";
                             var reply = new builder.Message()
                                 .setText(session, captionText)
                                 .addAttachment({
-                                text: "100/100 Mbps Internet + Custom TV + Phone starting at $69.99/mo",
-                                title: session.userData.selectedOffer + " offer for Phone",
-                                titleLink: "http://www.verizon.com/home/fios-fastest-internet/",
-                                contentType: "image/jpeg",
-                                contentUrl: "http://www.verizon.com/cs/groups/public/documents/adacct/mob_ocw_ftp_ofr2_0403.png"
-                            });
+                                    text: "100/100 Mbps Internet + Custom TV + Phone starting at $69.99/mo",
+                                    title: session.userData.selectedOffer + " offer for Phone",
+                                    titleLink: "http://www.verizon.com/home/fios-fastest-internet/",
+                                    contentType: "image/jpeg",
+                                    contentUrl: "http://www.verizon.com/cs/groups/public/documents/adacct/mob_ocw_ftp_ofr2_0403.png"
+                                });
                             //session.send("50/50 Mbps Internet + Custom TV + Phone starting at $79.99/mo");
                             session.send(reply);
                             builder.Prompts.confirm(session, "Do you like to proceed?\nPlease confirm " + session.userData.name);
                             break;
+
                         default:
                             session.send("I am sorry, i did not understand your answser.");
                             session.replaceDialog("/showOffer");
@@ -189,13 +205,14 @@ bot.add("/showOffer", [
         }
     }
 ]);
-//dialog.onDefault(builder.DialogAction.send("I am sorry, i did not understand your answser"));
+
+
+dialog.onDefault(builder.DialogAction.send("I am sorry, i did not understand your answser"));
+
 //bot.listenStdin();
 var server = restify.createServer();
-server.use(bot.verifyBotFramework());
-//server.use(bot.verifyBotFramework({ appId: process.env.appId, appSecret: process.env.appSecret }));
+server.use(bot.verifyBotFramework({ appId: process.env.appId, appSecret: process.env.appSecret }));
 server.post("/api/messages", bot.listen());
 server.listen(process.env.port, function () {
     console.log("%s listening to %s", server.name, server.url);
 });
-//# sourceMappingURL=server.js.map
